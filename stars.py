7a4354b3f2496392e2b492539948df826c620632
ireneg <irene.garnelo.abellanas@uni-paderborn.de>
2022-11-02 11:42:17 +0100

    [Evaluation]: adapted script to first real data

--- stars.py
+++ stars.py
@@ -3,11 +3,13 @@ import csv
 import numpy as np
 
 # set necessary parameters
-ex_id = 0                   # Exercise ID
+ex_id = 1                   # Exercise ID
 text_file = "responses.csv" # Name of csv with STACK-data
+pos_survey = 6 # num of col -1
 
 # First pass of the file to determine unique IDs for students
 names = []
+includes_survey = []
 with open(text_file, newline='') as csvfile:
     rreader = csv.reader(csvfile)
     inc = False
@@ -29,12 +31,16 @@ with open(text_file, newline='') as csvfile:
     for row in rreader:
         if(inc):
             rest = row[8].split(";")
-            rating = rest[2]
-            stars = rating[rating.find("[")+1 : rating.find("]")].split(",")
-            comment = rating[rating.find("comment")+12:]
-            comment = comment[:comment.find("\\")]
-            newline = [names.index(row[0]+row[1]), ex_id, row[7], rest[3]] + stars + [comment]
-            lines.append(newline)
+            if (len(rest) > pos_survey):
+                if ("ratings" in rest[pos_survey]):
+                    rating = rest[pos_survey]
+                    stars = rating[rating.find("ratings")+11 : rating.find("comment")-4].split(",")
+                    comment = rating[rating.find("comment")+12:]
+                    comment = comment[:comment.find("submitted")-5]
+                    if (comment == ""):
+                        comment = "LEER"
+                    newline = [names.index(row[0]+row[1]), ex_id, row[7], rest[3]] + stars + [comment]
+                    lines.append(newline)
         inc=True
 
 # Write data from list into new csv
